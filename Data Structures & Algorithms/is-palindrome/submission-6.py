0b96e2b0b2bf4c93665e2d3102f41fdd6e50c220
Juan Fernández-Pérez <juan_j_fernandez@brown.edu>
2026-08-06 13:56:46 -0500

    Add: is-palindrome - submission-6

---
+++ Data Structures & Algorithms/is-palindrome/submission-6.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        txt = "".join(char.upper() for char in s if char.isalnum())
+
+        right = len(txt) - 1
+        left = 0
+        for char in txt:
+            if char != txt[right]:
+                return False
+            right -= 1
+            left += 1
+        return True
