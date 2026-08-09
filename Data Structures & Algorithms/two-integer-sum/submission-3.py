553501cf015efe3157a4cf8b96b59d3627cc3e80
Juan Fernández-Pérez <juan_j_fernandez@brown.edu>
2026-08-08 22:28:17 -0500

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        indexMap = {}
+        currIndex = 0
+
+        for n in nums:
+            if target - n in indexMap:
+                return [indexMap[target - n], currIndex]
+            else:
+                indexMap[n] = currIndex
+                currIndex += 1
+        return []
